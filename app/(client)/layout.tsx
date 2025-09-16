2cdf5a4c894e73f846c106fd405ddf384f400f85
kdandy <dandybdr@gmail.com>
2025-09-16 14:57:13 +0700

    fix

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -5,10 +5,10 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s - Shopcart online store",
-    default: "Shopcart online store",
+    template: "%s - Jualin online store",
+    default: "Jualin online store",
   },
-  description: "Shopcart online store, Your one stop shop for all your needs",
+  description: "Jualin online store, Your one stop shop for all your needs",
 };
 
 export default function RootLayout({
